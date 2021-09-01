9bbf8e85c7148842cc5a1e1399b4911b0ec45ac2
LUISA FERNANDA VERGARA SEPULVEDA <fernandavergara0218@gmail.com>
2021-09-01 15:44:24 -0500

    Add Entity

---
+++ TaskTimesAzure.Common/Responses/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TaskTimesAzure.Common.Responses
+{
+    public class Response
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime DateHour { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+    }
+}
---
+++ TaskTimesAzure.Functions/Entities/TaskEntity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTimesAzure.Functions.Entities
+{
+     public class TaskEntity:TableEntity
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime DateHour { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidate { get; set; }
+
+    }
+}
